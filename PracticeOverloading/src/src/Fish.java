d61b2ee0f68a48c6270237377b3b3bbfdd0ea594
tedodidi <tedodidi@DESKTOP-Q5Q4ILU>
2022-04-03 22:22:17 +0300

    update

--- PracticeOverloading/src/src/Fish.java
+++ PracticeOverloading/src/src/Fish.java
@@ -3,6 +3,7 @@ package src;
 public class Fish {
 	private String typeOfFish;
 	private int friendliness;
+	private String friendlinessEvo;
 
 	public Fish() {
 		typeOfFish = "Unknown";
@@ -10,11 +11,42 @@ public class Fish {
 
 	}
 	public Fish(String t, int f) {
-		typeOfFish = t;
-		friendliness = f;
+		this.typeOfFish = t;
+		this.friendliness = f;
+
 	}
-	
+
 	public int getFriendliness() {
 		return friendliness;
 	}
+	
+	public String getFriendliness1() {
+		switch(friendliness) {
+		case 1: 
+			this.friendlinessEvo = "Mean";
+			break;
+		case 2: 
+			this.friendlinessEvo = "Not friendly";
+			break;
+		case 3: 
+			this.friendlinessEvo  = "Neutral";
+			break;
+		case 4: 
+			this.friendlinessEvo = "friendly";
+			break;
+		case 5: 
+			this.friendlinessEvo = "Very friendly";
+			break;
+		default:System.out.println("Error");
+		
+		}
+		return this.friendlinessEvo;
+	}
+	
+	
+	public String toString() {
+		return "Fish type "+this.typeOfFish
+				+"\nFriendliness: "+getFriendliness1();
+	}
+	
 }
---
+++ PracticeOverloading/src/src/FishDriver.java
@@ -0,0 +1,11 @@
+package src;
+
+public class FishDriver {
+
+	public static void main(String[] args) {
+		Fish amber = new Fish("AngelFish", 5);
+		System.out.println(amber);
+
+	}
+
+}
