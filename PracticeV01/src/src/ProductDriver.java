89107347165323a29b740983e4059035a10dd7a9
tedodidi <tedodidi@DESKTOP-Q5Q4ILU>
2022-04-04 20:23:09 +0300

    Update

--- PracticeV01/src/src/ProductDriver.java
+++ PracticeV01/src/src/ProductDriver.java
@@ -6,10 +6,10 @@ public class ProductDriver {
 	public static void main(String[] args) {
 		Scanner maxProducts1 = new Scanner(System.in);
 		System.out.println("Please enter how many products you wish to add as an integer number");
-		int maxProducts = maxProducts1.nextInt();
+		int maxProducts = maxProducts1.nextInt();// how many products you wish to add ?
 
 		Product[] myProducts = new Product[maxProducts];
-		String controlvar="Yes";
+	
 
 		for(int i = 0;i<myProducts.length;i++) {
 
@@ -29,31 +29,59 @@ public class ProductDriver {
 
 
 				myProducts[i] = new Product(name1,number1,quantity1,price1);
-				Scanner controls = new Scanner(System.in);
-				System.out.println("Enter \"No\"to stop the program ");
-				controlvar = controls.nextLine();
-				if(controlvar.equals("No"))
+
+				Scanner choice1 = new Scanner(System.in); // menu controls beginning
+				System.out.println("1. View Inventory"
+						+"\n2. Add Stock"
+						+"\n3. Deduct Stock"
+						+"\n4. Discontinue Stock"
+						+"\n0. Exit");
+				int choice = choice1.nextInt();
+				if(choice==0)
 					break;
+
+				switch(choice) {
+				case 1:
+					displayInventory(myProducts);
+					break;
+				case 2:
+					System.out.println("set total stock available");
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+		        default:
+					System.out.println("Wrong choice.");
+				}// menu controls end
+
 
 			}
 			catch(Exception E) {
 				System.out.println("Your data is inserted wrong");
 			}
-		}
-		Scanner display1 = new Scanner(System.in);
-		System.out.println("Do you wish to display products content ?If yes please type \"SHOW\"");
-		String display = display1.nextLine();
-		if(display.equals("SHOW"))
-			displayInventory(myProducts);
+		}// end loop that will add products
+
+
+
 	}
 
 
 	static void displayInventory(Product[] myProducts) {
 		for(int g=0;g<myProducts.length;g++) {
 			System.out.println("\n"+myProducts[g]);
-		}
-
+		}//end method for display of products
 	}
+
+
+	static void addInventory(Product[]myProducts,Scanner in) {
+		int numInput = in.nextInt();
+		Scanner quantityScan = new Scanner(System.in);
+		System.out.println("Please enter quantity to add to inventory");
+		int quantityAdded = quantityScan.nextInt();
+		myProducts[numInput].setQuantity(quantityAdded);
+	}
+
 }
 
 
